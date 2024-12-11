eb2220d43ba3be519ad008cba3f368c2d974830c
Standlm <128933013+Standlm@users.noreply.github.com>
2024-12-10 23:58:25 -0800

    defintly copied the wrongs things back into my game and gamestate

--- Game/src/main/java/Game.java
+++ Game/src/main/java/Game.java
@@ -1,22 +1,200 @@
-import java.util.Random;
 import java.util.List;
+import java.util.Scanner;
 
-public class Animal extends Item {
-    int min;
-    int max;
-    private Random rn;
+public class Game {
 
-    public Animal(String name, List<String> type, String desc, String use, String act, int min_damage, int max_damage) {
-        super(name, type, desc, use, act);
-        min = min_damage;
-        max = max_damage;
-        rn = new Random();
+    static String name;
+    static int choice;
+    static String itemp;
+
+
+    // helper function for printing
+    private static void printSlow(String toPrint) {
+        char[] chars = toPrint.toCharArray();
+        for (int i=0; i < chars.length; i++) {
+            System.out.print(chars[i]);
+            try { Thread.sleep(25);} 
+            catch (InterruptedException e) {Thread.currentThread().interrupt();}
+        }
+        System.out.println("");
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 
-    // uniformly distributed random number
-    public int attack() {
-        int var = min + rn.nextInt((max-min) + 1);
-        return var;
-    }
+    public static void main(String[] args) {
 
+        // only instantiate once
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        name = myObj.nextLine();
+        // init game state
+        GameState state = new GameState(name);
+        printSlow("Hello " + name);
+
+        // beginning flavor text
+        /**
+        printSlow("Welcome, "+name+".");
+        System.out.println("");
+        printSlow("You've been studying in the library for hours and decide to take a break by walking around.");
+        System.out.println("");
+        printSlow("You go downstairs into the basement, find an archive room, and get distracted by an old book describing the first version of Java (\'The Java Tutorial\' by Mary Campione and Kathy Walrath, published in 1997).");
+        System.out.println("");
+        printSlow("After reading for a while, you look up and notice that the room looks... different. The lighting seems a little dimmer, the room smells of cigarettes, and you could have sworn the carpet was a different pattern when you first walked into this room.");
+        */
+        while (!state.finished) {
+            System.out.println("");
+            System.out.println("What do you want to do next? \n" + name +":   health: " + state.health );
+            System.out.println("[1]: Look around the room.");
+            System.out.println("[2]: Move to a new room.");
+            System.out.println("[3]: Interact with something in the room.");
+            System.out.println("[4]: Examine my inventory.");
+            System.out.println("[5]: Use an object from my inventory.");
+
+            choice = myObj.nextInt();
+            myObj.nextLine(); // consume newline from above
+
+            switch (choice) {
+                case 1:
+                    printSlow("You can see the following items:");
+
+                    for (Item c : state.room.contents) printSlow(c.name);
+                    
+                    printSlow("You also notice that this room has doors:");
+                    for (String c : state.room.doors.keySet()) printSlow(c);
+                    break;
+            case 2:
+            
+                    printSlow("Which door?");
+                    String door = myObj.nextLine();
+    
+                    //checking if the next door is Heavy our locked 
+                    if (state.room.doors.containsKey(door)) {
+                        if("heavy".equals(door)){
+                            if(state.inventory.contains(state.items.get("Filled Sacred Container")) && state.items.get("Filled Sacred Container").isUsed()){
+                                String nextRoomName = state.room.doors.get(door);
+                                state.room = state.rooms.get(nextRoomName);
+                                printSlow("You step through the " + door + " door and enter the " + state.room.name + ".");
+                                break;
+                                }
+                            
+                            
+                            //if its heavy door its locked unless Players inventory cains Key
+                            printSlow("this door is locked");
+                            printSlow("you can't get in there");
+                            break;
+                        } 
+                            String nextRoomName = state.room.doors.get(door);
+                            state.room = state.rooms.get(nextRoomName);
+                            printSlow("You step through the " + door + " door and enter the " + state.room.name + ".");
+                        }  else{
+                            printSlow("Thats not a door");
+
+                        
+                            }
+                    
+                    
+                    break;
+                case 3:
+                    printSlow("Which item?");
+                    itemp = myObj.nextLine();
+                    
+                    
+            //types to name
+                    
+                    try {
+                        Item item = state.items.get(itemp);
+                        List<ItemType> types = item.getType();
+                        if(types.contains(ItemType.Table) || types.contains(ItemType.NPC)){
+                            printSlow(item.desc);
+                            printSlow("You cant pick " + item.name + " up");
+                            if(types.contains(ItemType.Table )){
+                                printSlow(item.use);
+
+                            }else
+                            
+                            printSlow(item.dialogue(state));
+                            break;
+
+
+                        }
+
+                        
+                        
+                        state.rooms.put(state.room.name, state.room);
+
+
+                        //we could infinetely pick up items this is an if else statement to fix that
+                        if(state.room.contents.contains(item)){
+                        state.inventory.add(item);
+                        state.room.contents.remove(item);
+                        printSlow("You pick up the " + item.name + ". " + item.desc + ".");
+                        }else{
+                            printSlow("Thats not in this ");
+                        }
+                       
+                       
+                    } catch (Exception e) {
+                        printSlow("Unknown item.");
+                    }
+                    break;
+                case 4:
+                    printSlow("Your inventory:");
+                    printSlow(state.inventory.toString());
+                    break;
+
+
+
+
+
+
+
+                    ///you have to action 
+                case 5:
+                    printSlow("Which item?");
+                    itemp = myObj.nextLine();
+                    try {
+                        Item item = state.items.get(itemp);
+                        if (state.inventory.contains(item)) {
+                            item.use();
+                            printSlow(item.use);
+                            if (item.action.equals("drop")) {
+                                state.inventory.remove(item);
+                                state.room.contents.add(item);
+                                state.rooms.put(state.room.name, state.room);
+                            }
+                            //if you are using an healing item it goes to the healing class
+                            if(item.action.equals("heal")){
+                                state.inventory.remove(item);
+                                state.health += Healing.healthEffect;
+                                state.rooms.put(state.room.name, state.room);
+                            }
+            
+                            
+                        }
+                        else {
+                            printSlow("Unknown item.");
+                        }
+                    } catch (Exception e) {
+                        printSlow("Unknown item.");
+                    }
+                    break;
+            }
+
+            String update = state.update();
+            printSlow(update);
+        }
+        if(state.health <= 0){
+            printSlow("You Lose");
+        }
+        
+        if(state.health  > 0){
+            printSlow("You win!");
+        }
+        
+    }
 }
+
--- Game/src/main/java/GameState.java
+++ Game/src/main/java/GameState.java
@@ -1,22 +1,128 @@
-import java.util.Random;
-import java.util.List;
+import java.util.*;
 
-public class Animal extends Item {
-    int min;
-    int max;
-    private Random rn;
+// Tracks global game state
+// Useful for implementing state-based behavior (ex: see something new on second visit to room)
 
-    public Animal(String name, List<String> type, String desc, String use, String act, int min_damage, int max_damage) {
-        super(name, type, desc, use, act);
-        min = min_damage;
-        max = max_damage;
-        rn = new Random();
+public class GameState {
+    HashMap<Room, Boolean> visited = new HashMap<Room, Boolean>();
+    String name;
+    boolean finished;
+    Room room;
+    List<Item> inventory = new ArrayList<Item>();
+    Map<String, Room> rooms; // global list of rooms
+    Map<String, Item> items; // global list of known items
+    int health = 10;
+    Room currentRoom;
+    // update state and check for winning condition
+    public String update() {
+        if(health <= 0){
+            finished = true;
+             String deathtext = """
+                     it seems like your health droped below zero I dont really know how that happened but your probably gonne need to run gradle again
+                     """;
+                     return deathtext;
+
+
+
+        }
+        if (room.contents.contains(items.get("poison frog")) &&
+            room.contents.contains(items.get("book")) ){
+            finished = true;
+       
+            String finaltext =  """
+                                The frog hops slowly over to the book and hops on top. Suddenly the book and the
+                                frog begin to glow. The room starts spinning and you shut your eyes out of fear.
+                                When you open them, you're back in the original basement room! When you open the
+                                door, you find yourself back in the modern-day library. As you leave and the door
+                                swings shut, you think you hear a faint \"ribbet\"....
+                                """;
+            return finaltext;
+        }
+        
+   
+        if(room.contents.contains(items.get("red pole")) &&
+            room.contents.contains(items.get("Unnamed Kid")) ) {
+                finished = false;
+                String text = """
+                            'thanks for getting my  red power pole for me, I kinda lost it and mr.popos gets real mad when i lose things'
+                            the young boy reaches  around and grabs a small empty bottle from his pocket and hands it to you
+                            'this might help you drink the water in the fountain without getting mr.popo mad'
+                            the young boy shivers at the idea of upsetting his gracious teacher
+
+                            You Have Recieved the: Empty Sacred Container
+                            
+                            
+                            """;
+
+                            inventory.add(items.get("Empty Sacred Container"));
+                            room.contents.remove(items.get("red pole"));
+                
+                        
+                       
+              return text;
+            }
+            if(room.contents.contains(items.get("Empty Sacred Container")) &&
+            room.contents.contains(items.get("Holy Fountain")) ) {
+                finished = false;
+                String text = """
+                        as you place this empty container into the fountain you see it fill it up with this clean crystaline water, drinking this might give you the chance to open that heavy door
+                        
+
+                        You have Recieved the: Filled Sacred Container
+                        """;
+                            
+                            
+                            
+                            
+
+                            inventory.add(items.get("Filled Sacred Container"));
+                            room.contents.remove(items.get("Empty Sacred Container"));
+                        
+                       
+              return text;
+            }
+            if(room.contents.contains(items.get("Dragon Balls")) && 
+            room.contents.contains((items.get("book") ))){
+                finished = true; 
+                String wish ="""
+                                    As you say the words rm rf a mighy dragon rises in golden glory from the dragon balls, mr pop laughs in the corner because he is current reading an
+                                    xtcf comic about Tar Commands and knows you probably wouldnt understand it.
+                                    the green dragon adorned with orange scales and mighty horns and angelic strands of hair understand your wish and your need to go home
+                                    the dragons eyes begin to glow and you reappear back at your desk at the Csci Lab.
+                        
+                        """;
+                        return wish;
+
+            }
+       /*  if(items.get("Empty Glass Vial").isUsed() && room.contents.contains(items.get"Holy Fountain")){
+            finished = false;
+            String text "You put the";
+*/
+        
+        return "";
+    }
+    public Room getCurrentRoom() {
+        return currentRoom;
     }
 
-    // uniformly distributed random number
-    public int attack() {
-        int var = min + rn.nextInt((max-min) + 1);
-        return var;
+    // Use an item in the current room (apply healing or unlock door)
+    public void useItem(Item item) {
+        item.use(this);  // Pass the current game state to the item's use method
     }
 
+    // Set the current room
+    public void setCurrentRoom(Room room) {
+        currentRoom = room;
+    }
+
+    public GameState(String name) {
+        this.name = name;
+        finished = false;
+        LoadYAML yl = new LoadYAML();
+        rooms = yl.rooms;
+        items = yl.items;
+        room = rooms.get("Starting Room");
+        visited.put(room, true);
+        inventory.add(items.get("book"));
+    }
 }
