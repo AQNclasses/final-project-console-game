952a4c0ae19d8c529a0a17e49dafa9d52405721e
Alli <anilles137@gmail.com>
2024-12-16 19:11:06 -0800

    Project feedback

--- Game/src/main/java/Weapon.java
+++ Game/src/main/java/Weapon.java
@@ -2,9 +2,9 @@ import java.util.Random;
 import java.util.List;
 
 public class Weapon extends Item {
-    int min;
-    int max;
-    private Random rn;
+    static int min;
+    static int max;
+    static private Random rn;
 
     public Weapon(String name, List<String> types, String desc, String use, String act, int min_damage, int max_damage) {
         super(name, types, desc, use, act);
@@ -14,7 +14,7 @@ public class Weapon extends Item {
     }
 
     // uniformly distributed random number
-    public int attack() {
+    public static int attack() {
         int var = min + rn.nextInt((max-min) + 1);
         return var;
     }
